69cc6a43e295edba77e37a0808355db7466036e6
Shannon Chang <changxshannon@gmail.com>
2016-10-04 14:46:14 -0700

    Saved commands to generate summary stats, histograms, and output files

---
+++ code/eda-script.R
@@ -0,0 +1,45 @@
+# ==============================================================================
+# Read in Advertising.csv data set
+# ==============================================================================
+advertising <- read.csv(file = "data/Advertising.csv", 
+                         row.names = 1)
+
+
+
+# ==============================================================================
+# Summary statistics
+# ==============================================================================
+# sink output
+sink(file = "data/eda-output.txt")
+# Summary statistics for TV and Sales
+summary(advertising[c(1, 4)])
+# STOP
+sink()
+
+
+# ==============================================================================
+# Histograms
+# ==============================================================================
+# TV
+hist(advertising$TV, 
+     xlab = "$ (in thousands)", 
+     ylim = c(0,50), 
+     main = "Histogram of Television Advertising Budgets")
+# Save as PNG
+dev.copy(png, "images/histogram-tv.png")
+dev.off()
+# Save as PDF
+dev.copy(pdf, "images/histogram-tv.pdf")
+dev.off()
+
+# Sales
+hist(advertising$Sales, 
+     xlab = "Product Sales (thousands of units)", 
+     ylim = c(0,50), 
+     main = "Histogram of Product Sales")
+# Save as PNG
+dev.copy(png, "images/histogram-tv.png")
+dev.off()
+# Save as PDF
+dev.copy(pdf, "images/histogram-tv.pdf")
+dev.off()
